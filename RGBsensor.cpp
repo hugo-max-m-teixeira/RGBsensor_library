c1d8019ce4aa615a9ec1149fec3f800479e1ae75
hugo-max-m-teixeira <hugomaxmontelesteixeira@gmail.com>
2022-07-17 18:55:44 -0300

    not tested yet, now color need to have a variation to be non-blank

--- RGBsensor.cpp
+++ RGBsensor.cpp
@@ -8,9 +8,7 @@
 
 #include <RGBsensor.h>
 
-void RGBsensor::setLDRpin(uint8_t pin){
-	pin_ldr = pin;
-}
+void RGBsensor::setLDRpin(uint8_t pin){	pin_ldr = pin;	}
 
 void RGBsensor::setRGBpins(uint8_t pin_r, uint8_t pin_g, uint8_t pin_b){
 	pin_led[0] = pin_r;	pin_led[1] = pin_g;	pin_led[2] = pin_b;
@@ -29,70 +27,104 @@ char RGBsensor::getColor(){
 	return color;
 }
 
-char RGBsensor::getRefletanceOrder(uint8_t pos){
-	return refletance_order[pos];
+char RGBsensor::getRefletanceOrder(uint8_t pos){	return refletance_order[pos];	}
+
+int RGBsensor::getR(){	return color_value[0];	}
+
+int RGBsensor::getG(){	return color_value[1];	}
+
+int RGBsensor::getB(){	return color_value[2];	}
+
+int RGBsensor::getColor(char color){	return color_value[charToIndex(color)];	}
+
+int RGBsensor::getColor(int index){	return color_value[index];	}
+
+int RGBsensor::getBlank(char color){	return blank_value[charToIndex(color)];	}
+
+int RGBsensor::getBlank(int index){	return blank_value[index];	}
+
+void RGBsensor::setBlank(char color, int value){	 blank_value[charToIndex(color)] = value;	}
+
+void RGBsensor::setBlank(int index, int value){	 blank_value[index] = value;	}
+
+int RGBsensor::getPerCent(char color){	return percent_value[charToIndex(color)];	}
+
+int RGBsensor::getPerCent(int index){	return percent_value[index];	}
+
+void RGBsensor::setCutoff(uint16_t val){	cutoff_percent_value = val;	}
+
+void RGBsensor::setMinColorVariatin(uint16_t val){	min_color_variation = val;	}
+
+void RGBsensor::setHighTime(uint16_t time){	high_time = time;	}
+
+void RGBsensor::setLowTime(uint16_t time){	low_time = time;	}
+
+void RGBsensor::setBlackPercentage(float percentage) {	black_percentage = 1.0 - (percentage/100.0);	}
+
+void RGBsensor::commonAnode (){	this->common_anode = true;	}
+
+void RGBsensor::turn(char color, bool state){	turn(charToIndex(color), state);	}
+
+void RGBsensor::turn(int color_num, bool state){
+	if (common_anode) state = !state;
+	digitalWrite(pin_led[color_num], state);
 }
 
-int RGBsensor::getR(){
-	return color_value[0];
-}
+void RGBsensor::setMultiplierBlank(float multiplier){	multiplier_blank = multiplier;	}
 
-int RGBsensor::getG(){
-	return color_value[1];
-}
-
-int RGBsensor::getB(){
-	return color_value[2];
-}
-
-int RGBsensor::getColor(char color){
-	return color_value[charToIndex(color)];
-}
-
-int RGBsensor::getBlank(char color){
-	return blank_value[charToIndex(color)];
-}
-
-int RGBsensor::getPerCent(char color){
-	int max_refletance = blank_value[charToIndex(color)];	//Gets the blank value
-	return (float(getColor(color)) / float(max_refletance)) * 100.0;	// Compare
-}
-
-void RGBsensor::setCutoff(uint16_t val){
-	cutoff_value = val;
-}
-
-void RGBsensor::setHighTime(uint16_t time){	//Time that the LED keeps ON
-	high_time = time;
-}
-
-void RGBsensor::setLowTime(uint16_t time){	//Time that the LED keeps OFF
-	low_time = time;
-}
-
-void RGBsensor::commonAnode (){
-	this->common_anode = true;
+bool RGBsensor::isBlack(){
+	bool black = true;
+	for(int i=0; i<3; i++){
+		if(color_value[i] > (blank_value[i] * black_percentage)){ // Try to false the variable, verifaca se algum pasa do valor esperado
+			/*Serial.println("\t\tValor da cor: " + String(color_value[i]));
+			Serial.println("\t\tValor da comparação: " + String((blank_value[i] * black_percentage)));
+			Serial.println("\t\tValor percentage: " + String(black_percentage));*/
+			black = false; 
+		}
+	}
+	return black;
 }
 
 void RGBsensor::setBlank(){
+	turn(2, 1);
+	delay(500);
+	turn(2, 0);
 	for(int i=0; i<3; i++){	// For each LED
-		digitalWrite(pin_led[i], (common_anode ? LOW : HIGH));	// turn on the led "i"
-		delay(high_time);										// Wait a little moment (*1.8)
+		turn(i, 1);												// turn on the led "i"
+		delay(high_time*multiplier_blank);										// Wait a little moment (*1.8)
 		blank_value[i] = analogRead(pin_ldr);					// Read the light(refletance) value
-		digitalWrite(pin_led[i], (common_anode ? HIGH : LOW));	// Turn off the led "i"
-		delay(low_time);										// Wait a moment while the LED stops emitting light
+		turn(i, 0);												// Turn off the led "i"
+		delay(low_time*multiplier_blank);										// Wait a moment while the LED stops emitting light	
 	}
+	last_lecture = millis();
+	total_lecture_time = high_time + low_time;
 }
 
 void RGBsensor::readColor(){
 	for(int i=0; i<3; i++){	// For each LED
-		digitalWrite(pin_led[i], (common_anode ? LOW : HIGH));	// turn on the led "i"
-		delay(high_time);										// Wait a little moment
+		turn(i, 1);												// turn on the led "i"
+		delay(computeDelay(millis(), last_lecture, high_time));										// Wait a little moment
 		color_value[i] = analogRead(pin_ldr);					// Read the light(refletance) value
-		digitalWrite(pin_led[i], (common_anode ? HIGH : LOW));	// Turn off the led "i"
-		delay(low_time);										// Wait a moment while the LED stops emitting light
+		turn(i, 0);												// Turn off the led "i"
+		delay(computeDelay(millis(), last_lecture, low_time));										// Wait a moment while the LED stops emitting light
+		
 	}
 	compareValues();	
+	last_lecture = millis();
+}
+
+unsigned int RGBsensor::computeDelay(unsigned long actual_time, unsigned long last_time, unsigned int default_delay){
+	unsigned int result;
+	unsigned int delta_time = actual_time - last_time;
+	if(delta_time < (total_lecture_time*3)){
+		result = default_delay;
+	} else {
+		result = default_delay * (delta_time/3);
+		if(result > (total_lecture_time*multiplier_blank)){
+			result = default_delay*multiplier_blank;
+		}
+	}
+	return result;
 }
 
 void RGBsensor::compareValues() {	
@@ -100,43 +132,51 @@ void RGBsensor::compareValues() {
 		compared_value[i] = blank_value[i] - color_value[i];
 	}
 	
+	computePerCent();
+	
+	int lower = percent_value[0];
+	for(int i:percent_value){	// Look for the lower value of the array
+		if(i < lower) lower = i;
+	}
+	
+	int higher = percent_value[0];
+	for(int i:percent_value){	// Look for the higher value of the array
+		if(i > higher)	higher = i;
+	}
+	
+	int second_lower = percent_value[0];
+	for(int i:percent_value){	// Middle value
+		if((i != lower) && (i != higher))	second_lower = i;
+	}
+	
 	bool white = true;	// Checks if the color is white (difference too small betwen blank_value and color_value)
-	for(int i:compared_value){
-		if(abs(i) > cutoff_value) white = false;
+	for(int i:percent_value){
+		if((abs(i) > cutoff_percent_value) && ((higher - lower) >= min_color_variation)) white = false;
 	}
 	
 	if(white){
 		color = 'W';
 		for(int i=0; i<3; i++) refletance_order[i] = 'W';
-	} else {
-		int lower = compared_value[0];
-		for(int i:compared_value){	// Look for the lower value of the array
-			if(i < lower) lower = i;
-		}
-		
-		int higher = compared_value[0];
-		for(int i:compared_value){	// Look for the higher value of the vector
-			if(i > higher)	higher = i;
-		}
-		
-		int second_lower = compared_value[0];
-		for(int i:compared_value){	// Middle value
-			if((i != lower) && (i != higher))	second_lower = i;
-		}
-		
-		color = numberToColor(lower);
+	} else {		
+		color = numberPerCentToColor(higher);
 
 		refletance_order[0] = color;
-		refletance_order[1] = numberToColor(second_lower);
-		refletance_order[2] = numberToColor(higher);
+		refletance_order[1] = numberPerCentToColor(second_lower);
+		refletance_order[2] = numberPerCentToColor(lower);
 	}
 }
 
-char RGBsensor::numberToColor (int value){
+void RGBsensor::computePerCent(){
+	for(int i=0; i<3; i++){
+		percent_value[i] = (double(color_value[i]) / double(blank_value[i]) * 100.0);
+	}
+}
+
+char RGBsensor::numberPerCentToColor (int value){
 	char result;
-	if(value == compared_value[0]) result='R';
-	else if(value == compared_value[1]) result='G';
-	else if(value == compared_value[2]) result='B';
+	if(value == percent_value[0]) result='R';
+	else if(value == percent_value[1]) result='G';
+	else if(value == percent_value[2]) result='B';
 	return result;
 }
 
--- RGBsensor.h
+++ RGBsensor.h
@@ -14,14 +14,15 @@
 class RGBsensor {
 public:
 	// Pins...
-	void setLDRpin(uint8_t pin);
-	void setRGBpins(uint8_t pin_r, uint8_t pin_g, uint8_t pin_b);
+	void setLDRpin(uint8_t pin);	//Sets the LDR pin (analog Arduino port)
+	void setRGBpins(uint8_t pin_r, uint8_t pin_g, uint8_t pin_b);	// Sets the RGB pins, respectively
 	void setPins();	// pinMode and etc.
 	
 	
 	//Basic methods
 	void setBlank();	// Set the blank color
 	char getColor();	// Read and return the strongest actual color ('R', 'G' or 'B')
+	bool isBlack();
 	
 	// Other methods
 	void readColor();			// Read values of actual color
@@ -29,23 +30,40 @@ public:
 	int getR();	//Returns the refletance of the red color
 	int getG();	//Returns the refletance of the green color
 	int getB();	//Returns the refletance of the blue color
-	int getColor(char color);
+	int getColor(char color);	//Returns the refletance of the color sensor
+	int getColor(int index);
 	int getBlank(char color);
-	void setCutoff(uint16_t val);	//Sets the cutoff value, for white color detect
-	int getPerCent(char color);
+	int getBlank(int index);
+	void setBlank(char color, int value);
+	void setBlank(int index, int value);
+	void setCutoff(uint16_t val);		//Sets the cutoff value, for white color detect
+	void setBlackPercentage(float percent);	// Sets the min value for color reflectance to be considered non-black
+	void RGBsensor::setMinColorVariatin(uint16_t val);
+	int getPerCent(char color);			// Returns the percentage of the color variance compared to the blank color value
+	int getPerCent(int index);
 	void setHighTime(uint16_t time);	//Time that the LED keeps ON
 	void setLowTime(uint16_t time);		//Time that the LED keeps OFF
-	void commonAnode();	// Common anode LED (don't call this method if your LED is common cathod)
+	void commonAnode();					// Common anode LED (don't call this method if your LED is common cathod)
+	void turn(char color, bool state);	//Change manually the state of the LED 
+	void turn(int color_num, bool state);
+	void setMultiplierBlank(float multiplier);	// Sets the time multiplier for the first blank lecture
 
 private:
+	unsigned long last_lecture;	// Last lecture time
+	uint16_t black_percentage = 70, min_color_variation = 7;
+	unsigned int total_lecture_time;
+	float multiplier_blank = 4;	// Multiplier for first blank lecture
 	uint8_t pin_ldr;	//LDR pin (light sensor)
 	uint8_t pin_led[3];	//LEDs pins (pin_led[0] = red, pin_led[1] = green, pin_led[2] = blue)
-	int blank_value[3], color_value[3], compared_value[3], cutoff_value = 22;	// Light values
+	int blank_value[3], color_value[3], compared_value[3], percent_value[3];	// Light values
+	uint16_t cutoff_percent_value = 10;	// Cutoff value for white and color diferentiation
 	char color = "";	// Color => 'R' =  red,     'G' = green,     'B' = blue
 	char refletance_order[3] = {""};	// Descending order of refletance
 	void compareValues();		// Compare the values betwen white color and actual color
-	char numberToColor (int value);
+	void computePerCent();		// Compute percent variation
+	char numberPerCentToColor(int value);
 	uint8_t charToIndex(char color);
+	unsigned int computeDelay(unsigned long actual_time, unsigned long last_time, unsigned int default_delay);
 	uint16_t high_time = 50, low_time = 50;	//Default times HIGH and LOW
 	bool common_anode = false;
 };
